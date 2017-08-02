aa94455b4b3e9a91a09c2392ff7cf292f3560e07
Emmanuel Desprez <Manu@MacBook-Pro-de-Emmanuel.local>
2017-08-02 16:14:30 +0200

    Create mutli components

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Hello vous !</h1>
+        <input type="text"/>
+      </div>
+    )
+  }
+}
+
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-class App extends React.Component {
-  render() {
-    return(
-      <div>
-        <h1>Hello vous !</h1>
-        <input type="text"/>
-      </div>
-    )
-  }
-}
+import App from './components/App';
 
 
 render(
