afa6ca99aaafa2819f944d1c7908cd0ac2d72c4e
Emmanuel Desprez <Manu@MacBook-Pro-de-Emmanuel.local>
2017-08-02 15:38:20 +0200

    Update src dir

---
+++ src/listeImages.js
@@ -0,0 +1,38 @@
+module.exports = {
+  image1: {
+    title: 'Bug Windows',
+    link: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/18580821_828753523942913_1277615975851622400_n.jpg',
+   },
+  image2: {
+    title: 'New Balance',
+    link: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/18579798_594611320727638_7360222503819542528_n.jpg',
+   },
+  image3: {
+    title: 'Mic AT2020',
+    link: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/18579476_311673922602779_2372680955266596864_n.jpg',
+   },
+  image4: {
+    title: 'War Room',
+    link: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/18513666_120778061830160_5782935686084558848_n.jpg',
+   },
+  image5: {
+    title: 'Dragon Quest VIII',
+    link: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/18382351_315267812240527_6391273616939417600_n.jpg',
+   },
+  image6: {
+    title: 'Traitement acoustique',
+    link: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/e15/18161097_1313650752033681_2172383321466601472_n.jpg',
+   },
+  image7: {
+    title: 'Image 7',
+    link: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/e15/18161097_1313650752033681_2172383321466601472_n.jpg',
+   },
+  image8: {
+    title: 'Traitement acoustique disdivfhdv',
+    link: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/e15/18161097_1313650752033681_2172383321466601472_n.jpg',
+   },
+  image9: {
+    title: 'Traitement acoustique',
+    link: 'https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/e15/18161097_1313650752033681_2172383321466601472_n.jpg',
+   }
+};
