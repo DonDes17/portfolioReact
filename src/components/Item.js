28c01a0cadb658aff79917fd895523ed3e140685
Emmanuel Desprez <Manu@MacBook-Pro-de-Emmanuel.local>
2017-08-02 16:22:53 +0200

    Add image into component

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class Item extends Component {
   render() {
     return(
-      <div className="container">
-
+      <div>
+        <image src="https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/18382351_315267812240527_6391273616939417600_n.jpg" alt="image demo"></image>
+        <p>TEXTE</p>
       </div>
     )
   }
