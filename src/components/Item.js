67b506f5126c0a5840d31699ebb48bc20ef23686
Emmanuel Desprez <Manu@MacBook-Pro-de-Emmanuel.local>
2017-08-02 16:20:27 +0200

    Create Item component

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import Item from './Item';
 
-class App extends Component {
+class Item extends Component {
   render() {
     return(
       <div className="container">
-        <Item />
+
       </div>
     )
   }
 }
 
 
-export default App;
+export default Item;
