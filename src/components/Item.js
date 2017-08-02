826311f4611832b10e36b4a6babcc636da6796f5
Emmanuel Desprez <Manu@MacBook-Pro-de-Emmanuel.local>
2017-08-02 16:23:52 +0200

    update img

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,7 @@ class Item extends Component {
   render() {
     return(
       <div>
-        <image src="https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/18382351_315267812240527_6391273616939417600_n.jpg" alt="image demo"></image>
+        <img src="https://scontent-cdg2-1.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/18382351_315267812240527_6391273616939417600_n.jpg" alt="image demo"></img>
         <p>TEXTE</p>
       </div>
     )
